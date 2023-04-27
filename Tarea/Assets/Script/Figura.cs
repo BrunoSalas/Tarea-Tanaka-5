21513e13d01dff7b7e9ff5dd075d7c0946935a6c
bruno Salas <65439485+BrunoSalas@users.noreply.github.com>
2023-04-26 22:33:00 -0500

    Figura (Llamar dato de vuelta)

--- Tarea/Assets/Script/Figura.cs
+++ Tarea/Assets/Script/Figura.cs
@@ -1,7 +1,22 @@
 using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public abstract class Figura
+public abstract class Figura : MonoBehaviour
 {
     protected Button boton;
+
+    private Action<float> regresar;
+    private void Start()
+    {
+        boton.onClick.AddListener(() => regresar?.Invoke(CalcularArea()));
+    }
+
+    public void RegresarDato(Action<float> regresar)
+    {
+        this.regresar = regresar;
+    }
     public abstract float CalcularArea();
 }
